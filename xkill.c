7d942e8acd6aa36b3ad5a4a795967d45e58f8eea
Jesse Adkins <jesserayadkins@gmail.com>
2010-09-28 13:29:49 -0700

    Purge cvs tags.
    Change-Id: If02bd38d5c24154d2db8ba5c059743004f72e590
    Signed-off-by: Jesse Adkins <jesserayadkins@gmail.com>

--- xkill.c
+++ xkill.c
@@ -1,4 +1,3 @@
-/* $Xorg: xkill.c,v 1.5 2001/02/09 02:05:54 xorgcvs Exp $ */
 /*
 
 Copyright 1988, 1998  The Open Group
@@ -26,7 +25,6 @@ other dealings in this Software without prior written authorization
 from The Open Group.
 
 */
-/* $XFree86: xc/programs/xkill/xkill.c,v 1.5 2001/04/01 14:00:22 tsi Exp $ */
 
 /*
  * xkill - simple program for destroying unwanted clients
